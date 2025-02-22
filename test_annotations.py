b1a2df61159f6897a9707917d3424e08b1443f3c
Kalyan <archakamk@gmail.com>
2025-02-22 15:22:35 -0500

    removed some files

--- test_annotations.py
+++
@@ -1,7 +0,0 @@
-import json
-
-# Load and print the JSON file
-with open('/Users/kalyan/Desktop/VascuLens/EyeData/annotations.json', 'r') as f:
-    data = json.load(f)
-
-print(json.dumps(data, indent=4))  # Pretty-print the JSON content
